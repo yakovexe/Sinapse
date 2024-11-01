b723752b179d48f909e18a07b08c7d3012a94da1
diogobnfr <diogobonofre@gmail.com>
2024-11-01 00:55:27 -0300

    feat: /flashcard route initial GET/POST implementation

--- sinapse_api/src/main.rs
+++ sinapse_api/src/main.rs
@@ -1,3 +1,51 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpResponse, HttpServer};
+use serde::Deserialize;
+
+// TODO: MOVE TO MODEL DIR
+#[allow(dead_code)]
+#[derive(Debug, Deserialize)]
+struct Flashcard {
+    question: String,
+    answer: String,
+}
+
+// TODO: MOVE TO ROUTES DIR 
+async fn index() -> HttpResponse {
+    // PLACEHOLDER CODE
+    HttpResponse::Ok().body("INDEX")
+}
+
+async fn post_flashcard(flashcard: web::Json<Flashcard>) -> HttpResponse {
+    println!("Received flashcard: {:?}", flashcard);
+    HttpResponse::Created().body("Flashcard created!")
+}
+
+async fn get_flashcards() -> HttpResponse {
+    // PLACEHOLDER CODE
+    HttpResponse::Ok().body("GET")
+}
+
+// Bootstraps and initialize the application
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    println!("{}", format!("http://127.0.0.1:8080"));
+    HttpServer::new(|| {
+        // App Object
+        // - Routing
+        // - Shared Data
+        // - Separation of Concerns
+        App::new()
+            // .app_data(<app shared data/state>) goes here
+            .route("/", web::get().to(index))
+            .service(
+                web::scope("/v1").service(
+                    web::resource("/flashcard")
+                        .route(web::post().to(post_flashcard))
+                        .route(web::get().to(get_flashcards))
+                )
+            )
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }

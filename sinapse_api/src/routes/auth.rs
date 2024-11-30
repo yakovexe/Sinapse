17476cf51999f1d51889c7508fa834779f4b4cca
ya'akov <diogobonofre@gmail.com>
2024-11-30 15:41:02 -0300

    Chore: renaming user route to auth

--- sinapse_api/src/routes/auth.rs
+++ sinapse_api/src/routes/auth.rs
@@ -9,6 +9,9 @@ const USERS: &str = "users";
 
 #[post("/users")]
 async fn post_user(client: web::Data<Client>, Json(user): web::Json<User>) -> HttpResponse {
+    if user.password.is_empty() || user.username.is_empty() {
+        return HttpResponse::BadRequest().body("Username and password are required");
+    }
     let collection: Collection<User> = client.database(DATABASE).collection(USERS);
     match collection.insert_one(user).await {
         Ok(_) => HttpResponse::Created().body("User created!"),

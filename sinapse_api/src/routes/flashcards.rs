a100e24337a23c6ac0954fdddd25f673b1ecb1da
ya'akov <diogobonofre@gmail.com>
2024-11-30 18:30:47 -0300

    Feature: GET flashcards from specified deck

--- sinapse_api/src/routes/flashcards.rs
+++ sinapse_api/src/routes/flashcards.rs
@@ -1,21 +1,19 @@
-use std::result;
-
 use actix_web::{
     get, post,
     web::{self, Json},
     HttpResponse,
 };
-use bson::{doc, document, raw::Error, Document};
+use bson::{doc, Document};
 use futures_util::StreamExt;
 use mongodb::{Client, Collection};
 
-use crate::models::flashcard::{self, Flashcard};
+use crate::models::flashcard::Flashcard;
 
 const DATABASE: &str = "SinapseDB";
 const FLASHCARDS: &str = "flashcards";
 
 #[post("/flashcards")]
-async fn post_flashcard(
+pub async fn post_flashcard(
     client: web::Data<Client>,
     Json(flashcard): web::Json<Flashcard>,
 ) -> HttpResponse {
@@ -31,19 +29,16 @@ async fn post_flashcard(
 }
 
 // Solution based on https://stackoverflow.com/questions/67036017/how-to-get-collection-of-document-from-mongodb-cursor
-#[get("/flashcards")]
-async fn get_flashcards(client: web::Data<Client>) -> HttpResponse {
+#[get("/flashcards/{deck_id}")]
+pub async fn get_flashcards(client: web::Data<Client>, deck_id: web::Path<String>) -> HttpResponse {
     let database = client.database(DATABASE);
     let collection = database.collection::<Flashcard>(FLASHCARDS);
 
-    let filter: Document = doc! { "deck_id": { "$exists": true}, "question": { "$exists": true }, "answer": { "$exists": true } };
+    let filter: Document = doc! { "deck_id": deck_id.to_string() };
 
-    // Fetch all documents from the collection
     match collection.find(filter).await {
         Ok(mut cursor) => {
-            let mut flashcards = Vec::new();
-
-            // Collect all documents
+            let mut flashcards: Vec<Flashcard> = Vec::new();
             while let Some(result) = cursor.next().await {
                 match result {
                     Ok(document) => flashcards.push(document),
@@ -52,11 +47,9 @@ async fn get_flashcards(client: web::Data<Client>) -> HttpResponse {
                     }
                 }
             }
-
-            // Return the documents as JSON
             HttpResponse::Ok().json(flashcards)
         }
-        Err(err) => HttpResponse::InternalServerError().body(format!("Error: {}", err)),
+        Err(err) => return HttpResponse::InternalServerError().body(format!("Error: {}", err)),
     }
 }
 

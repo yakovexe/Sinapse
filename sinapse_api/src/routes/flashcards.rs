ddac41ad7684d9bdec43d49c90b76eedc6f78bc0
ya'akov <diogobonofre@gmail.com>
2024-11-30 15:46:22 -0300

    Refactor: improved post_flashcards for better parameter checking

--- sinapse_api/src/routes/flashcards.rs
+++ sinapse_api/src/routes/flashcards.rs
@@ -1,9 +1,15 @@
-use actix_web::{get, post, web, HttpResponse};
-use bson::{doc, Document};
+use std::result;
+
+use actix_web::{
+    get, post,
+    web::{self, Json},
+    HttpResponse,
+};
+use bson::{doc, document, raw::Error, Document};
 use futures_util::StreamExt;
 use mongodb::{Client, Collection};
 
-use crate::models::flashcard::Flashcard;
+use crate::models::flashcard::{self, Flashcard};
 
 const DATABASE: &str = "SinapseDB";
 const FLASHCARDS: &str = "flashcards";
@@ -11,16 +17,18 @@ const FLASHCARDS: &str = "flashcards";
 #[post("/flashcards")]
 async fn post_flashcard(
     client: web::Data<Client>,
-    flashcard: web::Json<Flashcard>,
+    Json(flashcard): web::Json<Flashcard>,
 ) -> HttpResponse {
+    if flashcard.deck_id.is_empty() || flashcard.question.is_empty() || flashcard.answer.is_empty()
+    {
+        return HttpResponse::BadRequest().body("deck_id, question and answer are required");
+    }
     let collection: Collection<Flashcard> = client.database(DATABASE).collection(FLASHCARDS);
-    match collection.insert_one(flashcard.into_inner()).await {
+    match collection.insert_one(flashcard).await {
         Ok(_) => HttpResponse::Created().body("Flashcard created!"),
         Err(err) => HttpResponse::InternalServerError().body(err.to_string()),
     }
 }
-
-// TODO: #[post("/flashcards/{deck_id}")]
 
 // Solution based on https://stackoverflow.com/questions/67036017/how-to-get-collection-of-document-from-mongodb-cursor
 #[get("/flashcards")]
@@ -28,7 +36,7 @@ async fn get_flashcards(client: web::Data<Client>) -> HttpResponse {
     let database = client.database(DATABASE);
     let collection = database.collection::<Flashcard>(FLASHCARDS);
 
-    let filter: Document = doc! { "question": { "$exists": true }, "answer": { "$exists": true } };
+    let filter: Document = doc! { "deck_id": { "$exists": true}, "question": { "$exists": true }, "answer": { "$exists": true } };
 
     // Fetch all documents from the collection
     match collection.find(filter).await {

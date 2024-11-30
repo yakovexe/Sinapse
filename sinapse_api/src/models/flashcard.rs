adad4523b3dbd39af0f973190afc65800e8fa51e
ya'akov <diogobonofre@gmail.com>
2024-11-30 15:42:33 -0300

    Fix: fixing Flashcard model visibility

--- sinapse_api/src/models/flashcard.rs
+++ sinapse_api/src/models/flashcard.rs
@@ -1,8 +1,8 @@
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize)]
-pub(crate) struct Flashcard {
-    deck_id: String,
-    question: String,
-    answer: String,
+pub struct Flashcard {
+    pub deck_id: String,
+    pub question: String,
+    pub answer: String,
 }
